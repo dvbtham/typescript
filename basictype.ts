97a7b48fe48d899f2293b70e6696ed0bb13be700
dvbtham <dvbtham@gmail.com>
2016-11-04 21:16:08 +0700

    the complete basic type with example in typecript

--- basictype.ts
+++ basictype.ts
@@ -2,7 +2,51 @@ let isDone: boolean = true;
 let number = 120;
 console.log(isDone);
 
-let fullName:string = "Thâm David";
-let sentense:string = `My name is ${fullName}`;
+let fullName: string = "Thâm David";
+let age: number = 19;
+let sentense: string = `My name is ${fullName}, I'm ${age + 1} years old.`;
 
 console.log(sentense);
+//Array//
+let myArray: number[] = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(myArray[3]);
+
+let employee: Array<string> = ["Tham", "Thinh", "Tri"];
+
+console.log(`This guy so manly: ${employee[0]}, 2 guys are gay: ${employee[1]} - ${employee[2]}`);
+
+//Tuple
+
+let customer: [number, string];
+customer = [123, "Thâm David"];
+
+console.log(`Id: ${customer[0]}, Customer name: ${customer[1]}`);
+
+//enum
+
+enum animals { Dog, Tiger, Cat };
+let animal:animals = animals.Cat
+
+console.log(`I have these animals: ${animals[0]} ${animals[1]} ${animals[2]}`);
+
+function warnUser(): void {
+    alert("This is my warning message");
+}
+//any
+
+
+let list: any[] = [1, true, "free"];
+
+list[1] = 100;
+list[2] = "aasf";
+list[0] = 123;
+console.log(`${list[1]} ${list[2]} ${list[0]}`);
+
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length;
+
+let strLength2: number = (someValue as string).length;
+
+console.log(`${strLength} ${strLength2}`);
--- dist/basictype.js
+++ dist/basictype.js
@@ -2,6 +2,39 @@ var isDone = true;
 var number = 120;
 console.log(isDone);
 var fullName = "Thâm David";
-var sentense = "My name is " + fullName;
+var age = 19;
+var sentense = "My name is " + fullName + ", I'm " + (age + 1) + " years old.";
 console.log(sentense);
+//Array//
+var myArray = [1, 2, 3, 4, 5, 6, 7];
+console.log(myArray[3]);
+var employee = ["Tham", "Thinh", "Tri"];
+console.log("This guy so manly: " + employee[0] + ", 2 guys are gay: " + employee[1] + " - " + employee[2]);
+//Tuple
+var customer;
+customer = [123, "Thâm David"];
+console.log("Id: " + customer[0] + ", Customer name: " + customer[1]);
+//enum
+var animals;
+(function (animals) {
+    animals[animals["Dog"] = 0] = "Dog";
+    animals[animals["Tiger"] = 1] = "Tiger";
+    animals[animals["Cat"] = 2] = "Cat";
+})(animals || (animals = {}));
+;
+var animal = animals.Cat;
+console.log("I have these animals: " + animals[0] + " " + animals[1] + " " + animals[2]);
+function warnUser() {
+    alert("This is my warning message");
+}
+//any
+var list = [1, true, "free"];
+list[1] = 100;
+list[2] = "aasf";
+list[0] = 123;
+console.log(list[1] + " " + list[2] + " " + list[0]);
+var someValue = "this is a string";
+var strLength = someValue.length;
+var strLength2 = someValue.length;
+console.log(strLength + " " + strLength2);
 //# sourceMappingURL=basictype.js.map
