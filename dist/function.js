f8434ac30b211b79dce21357111676ed1eef03b0
dvbtham <dvbtham@gmail.com>
2016-11-26 15:40:38 +0700

    arrow function

--- dist/function.js
+++ dist/function.js
@@ -5,4 +5,7 @@ var myAdd = function (x, y) {
     return x + y;
 };
 console.log(Add(12, 8), myAdd(12, 12));
+// arrow function
+var calculateSalary = function (bonus) { return 6000000 + bonus; };
+console.log(calculateSalary(100000));
 //# sourceMappingURL=function.js.map
--- function.ts
+++ function.ts
@@ -5,3 +5,8 @@ let myAdd = function (x: number, y: number): number {
     return x + y;
 }
 console.log(Add(12, 8), myAdd(12, 12));
+
+// arrow function
+let calculateSalary = (bonus: number) => 6000000 + bonus;
+
+console.log(calculateSalary(100000)); 
