be0577333527ce26e6c2cfe384811b33f4d0172f
dvbtham <dvbtham@gmail.com>
2016-11-28 21:41:17 +0700

    boc tach array

--- boctachArray.ts
+++ boctachArray.ts
@@ -1,4 +1,6 @@
 let myArr = ['Tham', 'Tri', 'Thinh'];
 
 let [tham, tri, thinh] = myArr;
+let [getone, ,] = myArr
 console.log(tham + " " + tri + " " + thinh);
+console.log(getone + " is the best.");
--- dist/boctachArray.js
+++ dist/boctachArray.js
@@ -1,4 +1,6 @@
 var myArr = ['Tham', 'Tri', 'Thinh'];
 var tham = myArr[0], tri = myArr[1], thinh = myArr[2];
+var getone = myArr[0];
 console.log(tham + " " + tri + " " + thinh);
+console.log(getone + " is the best.");
 //# sourceMappingURL=boctachArray.js.map
